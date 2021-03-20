8f54d6286a94cf573663f2d2a63a3a729071d292
lycho33 <cho.lyds@gmail.com>
2021-03-20 16:45:15 -0400

    updated environment.rb to include development

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,10 @@
-require "bundler/setup"
-Bundler.require
+ENV['SINATRA_ENV'] ||= "development"
 
-ActiveRecord::Base.establish_connection({
-    :adapter => "sqlite3",
-    :database => "db/development.sqlite3"
-})
-
+require 'bundler/setup'
+Bundler.require(:default, ENV['SINATRA_ENV'])
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+require './app/controllers/application_controller'
 require_all 'app'
